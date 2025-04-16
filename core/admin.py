091fe189d140aa6cbe7123b24016a9f92bb4469d
galindo <egalindo54@uabc.edu.mx>
2025-04-15 19:40:10 -0700

    todo eliminado

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,1 @@
 from django.contrib import admin
-
-from core.models import Ubicacion
-
-# Register your models here.
-@admin.register(Ubicacion)
-class UbicacionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'latitud', 'longitud')  # Campos a mostrar en la lista
-    search_fields = ('latitud', 'longitud')      # Campos por los que se puede buscar
-    list_per_page = 20     
--- core/forms.py
+++ core/forms.py
@@ -1,11 +1,1 @@
 from django import forms
-from .models import Ubicacion
-
-class UbicacionForm(forms.ModelForm):
-    class Meta:
-        model = Ubicacion
-        fields = ['latitud', 'longitud']
-        widgets = {
-            'latitud': forms.HiddenInput(),
-            'longitud': forms.HiddenInput(),
-        }
