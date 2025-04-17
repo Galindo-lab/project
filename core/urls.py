0c49691b3693ba5fce74e2301dbffe7bfe7d9e0f
galindo <egalindo54@uabc.edu.mx>
2025-04-17 14:04:58 -0700

    formulario de registro

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,4 @@
+from django import views
 from django.conf.urls.static import static
 from django.urls import path
 from django.urls import include
@@ -6,6 +7,7 @@ from django.contrib.auth import views as auth_views
 from project import settings
 
 from core.views import index
+from core.views import register
 
 urlpatterns = [
     path("", index, name="home"),
@@ -13,6 +15,7 @@ urlpatterns = [
     # Login
     path('login/', auth_views.LoginView.as_view(template_name='view/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
     
     # URLs para la pwa
     path('', include('pwa.urls')),
--- core/views.py
+++ core/views.py
@@ -1,11 +1,13 @@
 import json
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+
+from core.forms import RegisterForm
 
 def index(request):
     """
@@ -13,4 +15,18 @@ def index(request):
     """
     return render(request, 'layout/app.html')
 
+from django.shortcuts import render, redirect
 
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        # Si el formulario no es válido, se renderiza con los errores
+    else:
+        form = RegisterForm()  # Formulario vacío para GET requests
+    
+    return render(request, 'view/register.html', {
+        'form': form  # Pasa el formulario con errores si los hay
+    })
