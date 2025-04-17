de4f688b2e5a058780ef3ab6102e4d5c441717e9
galindo <egalindo54@uabc.edu.mx>
2025-04-17 16:01:16 -0700

    recuperar contraseña agregado

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,19 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
     
+    # URLs para el restablecimiento de contraseña
+    path(route='reset_password/', view=auth_views.PasswordResetView.as_view(
+        template_name='view/reset.html'), name='reset_password'),
+
+    path(route='reset_password_sent/', view=auth_views.PasswordResetDoneView.as_view(
+        template_name='view/resetDone.html'), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='view/resetConfirm.html'), name='password_reset_confirm'),
+
+    path(route='reset_password_complete/', view=auth_views.PasswordResetCompleteView.as_view(
+        template_name='view/resetComplete.html'), name='password_reset_complete'),
+    
     # URLs para la pwa
     path('', include('pwa.urls')),
 ]
