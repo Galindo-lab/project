7fd5942df916d0f911207be1c7b664f1fe889b7a
galindo <egalindo54@uabc.edu.mx>
2025-05-22 12:51:32 -0700

    qewqeq

--- scripts/create_users.sh
+++ scripts/create_users.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo docker compose exec -it web python manage.py shell -c "
+python manage.py shell -c "
 from django.contrib.auth import get_user_model;
 User = get_user_model();
 User.objects.create_superuser(username='test', email='test@example.com', password='123');
 for i in range(1, 20):
     User.objects.create_user(username=f'user{i}', email=f'user{i}@example.com', password='123');
-"
+    "
